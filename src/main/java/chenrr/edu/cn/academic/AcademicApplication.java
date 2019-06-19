339e449c1c5deda69b68e33d015fa3d0ff09f149
Rose329Chen <chenrrzr@163.com>
2019-06-19 20:25:59 +0800

    test hello,rr!

---
+++ src/main/java/chenrr/edu/cn/academic/AcademicApplication.java
@@ -0,0 +1,13 @@
+package chenrr.edu.cn.academic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcademicApplication.class, args);
+    }
+
+}
