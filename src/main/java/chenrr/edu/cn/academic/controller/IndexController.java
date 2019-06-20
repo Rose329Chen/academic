b32c3265adb5b6cb9f97a59bbe36f32f50bd9850
Rose329Chen <chenrrzr@163.com>
2019-06-20 10:45:41 +0800

    增加了 bootstrap 导航栏

---
+++ src/main/java/chenrr/edu/cn/academic/controller/IndexController.java
@@ -0,0 +1,12 @@
+package chenrr.edu.cn.academic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
